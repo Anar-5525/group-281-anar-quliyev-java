6454064e673fee34074fb970e91701fe03c5e425
Anar-5525 <anar44305@gmail.com>
2026-01-28 20:32:41 +0400

    homework_2

--- src/homework_2/Runner.java
+++ src/homework_2/Runner.java
@@ -1,10 +1,14 @@
 package homework_2;
 
+import homework_2.Homework_2.Student;
+
 public class Runner {
 	public static void main(String[] args){
 		
 		Student myStudent = new Student("Анна", 19, 4,8);
+
+		syso
+		
+		myStudent.printInfo();
 	}
-	
-	myStudent.printInfo();
 }
